17127fef78eaccd6186d29aba6af41473cf9ecc6
Victor <mirfaiziev.victor@gmail.com>
2018-08-26 15:45:22 +0200

    cleanup, add shared hosting preparation sh

---
+++ shared-deploy-prepare.sh
@@ -0,0 +1,7 @@
+rm -rf deployment;
+mkdir deployment;
+cp -r config public_html src templates translations vendor deployment;
+mkdir -p deployment/var ;
+cp .remote_config/.htaccess .remote_config/.htpasswd deployment/public_html
+zip -r deployment/vendor.zip deployment/vendor/
+zip -r deployment/code.zip config public_html src templates translations
